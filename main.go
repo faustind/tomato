201c5bdc825dfa36953eb8fd89642219cae0319c
Faustin Date <datefaustin@gmail.com>
2022-09-18 12:53:25 +0900

    Simple inline pomodoro timer

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
+type duration struct {
 	min int
 	sec int
 }
+type model struct {
+	duration            duration
+	durations           map[byte]duration
+	pattern             string
+	currentCountdownIdx int
+}
 
-func initialModel() *model {
+func initialModel(pattern string, w, s, l int) *model {
+	durations := map[byte]duration{'w': {w, 0}, 's': {s, 0}, 'l': {l, 0}}
+	start := pattern[0]
 	return &model{
-		min: 1,
-		sec: 0,
+		currentCountdownIdx: 0,
+		duration:            durations[start],
+		durations:           durations,
+		pattern:             pattern,
 	}
 }
 
@@ -37,16 +49,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case TickMsg:
-		if m.min == 0 && m.sec == 0 {
-			return m, nil
+		if m.duration.min == 0 && m.duration.sec == 0 {
+			// move to next counter in pattern
+			idx := m.currentCountdownIdx + 1
+			if idx > len(m.pattern)-1 {
+				idx = 0
+			}
+			m.currentCountdownIdx = idx
+
+			//set duration for current counter
+			m.duration = m.durations[m.pattern[m.currentCountdownIdx]]
+			return m, doTick()
 		}
 
-		min, sec := m.min, m.sec-1
+		min, sec := m.duration.min, m.duration.sec-1
 		if sec < 0 {
 			min, sec = min-1, 59
 		}
 
-		m.min, m.sec = min, sec
+		m.duration.min, m.duration.sec = min, sec
 		return m, doTick()
 
 	case tea.KeyMsg:
@@ -60,15 +81,39 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	s := fmt.Sprintf("tomato\n")
-	s += fmt.Sprintf("%2d:%2d", m.min, m.sec)
+	msgs := map[byte]string{
+		'w': "working session",
+		's': "short break",
+		'l': "long break",
+	}
+
+	s := fmt.Sprintf("tomato: %s\n", msgs[m.pattern[m.currentCountdownIdx]])
+	s += fmt.Sprintf("%02d:%02d", m.duration.min, m.duration.sec)
 	s += "\npress q to quit.\n"
 	return s
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
-	if err := p.Start(); err != nil {
+	var p = flag.String("p", "wswswl", "Pattern of a working session")
+	var w = flag.Int("w", 25, "Duration of a working session")
+	var s = flag.Int("s", 5, "Duration of a short break")
+	var l = flag.Int("l", 10, "Duration of a long break")
+
+	flag.Parse()
+	// validate flags
+	if *p == "" {
+		fmt.Printf("Invalid pattern ''%s', should not be empty\n", *p)
+		os.Exit(2)
+	}
+	for _, c := range *p {
+		if c != 'w' && c != 'l' && c != 's' {
+			fmt.Printf("Invalid pattern ''%s', should contain only w,s, or l\n", *p)
+			os.Exit(2)
+		}
+	}
+
+	prog := tea.NewProgram(initialModel(*p, *w, *s, *l))
+	if err := prog.Start(); err != nil {
 		log.Fatalf("There's been an error: %v", err)
 	}
 }
